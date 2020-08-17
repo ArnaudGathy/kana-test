b0d84c3ad0eafac32f04814ebc29392f0fa9de90
Arnaud <arno.firefox@gmail.com>
2020-08-17 20:43:05 +0200

    added select all

--- src/App.js
+++ src/App.js
@@ -4,22 +4,57 @@ import {hiraganaList, katakanaList} from './charLists'
 import './style.css'
 import {CharacterCheckBoxList} from './CharacterCheckBoxList'
 
+const emptyFormState = {
+  a: false,
+  ka: false,
+  sa: false,
+  ta: false,
+  na: false,
+  ha: false,
+  ma: false,
+  ya: false,
+  ra: false,
+  wa: false,
+  all: false,
+}
+
+const fullFormState = {
+  a: true,
+  ka: true,
+  sa: true,
+  ta: true,
+  na: true,
+  ha: true,
+  ma: true,
+  ya: true,
+  ra: true,
+  wa: true,
+  all: true,
+}
+
+const defaultFormState = {
+  hiragana: emptyFormState,
+  katakana: emptyFormState,
+}
+
 export const App = () => {
   const [list, setList] = useState(null)
   const [correction, setCorrection] = useState(false)
-  const [formState, setFormState] = useState({})
+  const [formState, setFormState] = useState(defaultFormState)
 
   const generateList = () => {
     const {hiragana, katakana} = formState
-    const hiraganaToDisplay = hiragana ? Object.keys(hiragana) : []
-    const katakanaToDisplay = katakana ? Object.keys(katakana) : []
 
-    const hiraganaDisplayList = hiraganaToDisplay.reduce((acc, value) => {
-      acc.push(hiraganaList[value])
+    const hiraganaDisplayList = Object.entries(hiragana).reduce((acc, [key, value]) => {
+      if(value && key !== 'all') {
+        acc.push(hiraganaList[key])
+      }
       return acc
     }, [])
-    const katakanaDisplayList = katakanaToDisplay.reduce((acc, value) => {
-      acc.push(katakanaList[value])
+    const katakanaDisplayList = Object.entries(katakana).reduce((acc, [key, value]) => {
+      if(value && key !== 'all') {
+        acc.push(katakanaList[key])
+      }
       return acc
     }, [])
 
@@ -29,24 +64,42 @@ export const App = () => {
     setCorrection(!correction)
   }
   const handleChange = (listName, value) => {
-    if (formState[listName] && formState[listName][value]) {
-      delete formState[listName][value]
-    } else {
-      const newFormState = {...formState, [listName]: {...formState[listName], [value]: true}}
-      setFormState(newFormState)
-    }
+    const newState = {...formState, [listName]: {...formState[listName], [value]: !formState[listName][value]}}
+    setFormState(newState)
+  }
+  const selectAll = (listName, isChecked) => {
+    const isSelected = formState[listName].all
+    setFormState({...formState, [listName]: isSelected ? emptyFormState : fullFormState})
+  }
+  const resetForm = () => {
+    setFormState(defaultFormState)
   }
 
   return (
     <div>
       <div className="block" style={{display: 'flex'}}>
-        <CharacterCheckBoxList name="hiragana" list={hiraganaList} handleChange={handleChange} />
-        <CharacterCheckBoxList name="katakana" list={katakanaList} handleChange={handleChange} />
+        <CharacterCheckBoxList
+          name="hiragana"
+          list={hiraganaList}
+          handleChange={handleChange}
+          formValues={formState.hiragana}
+          selectAll={selectAll}
+        />
+        <CharacterCheckBoxList
+          name="katakana"
+          list={katakanaList}
+          handleChange={handleChange}
+          formValues={formState.katakana}
+          selectAll={selectAll}
+        />
       </div>
 
       <div className="block">
         <button type="submit" onClick={generateList}>
-          Go
+          Générer la liste
+        </button>
+        <button type="submit" onClick={resetForm}>
+          Reset
         </button>
       </div>
 
--- src/CharacterCheckBoxList.js
+++ src/CharacterCheckBoxList.js
@@ -1,15 +1,19 @@
 import React from 'react'
 
-export const CharacterCheckBoxList = ({name, list, handleChange}) => (
+export const CharacterCheckBoxList = ({name, list, handleChange, formValues, selectAll}) => (
   <div>
     <p style={{textTransform: 'capitalize'}}>{name}</p>
     {Object.entries(list).map(([key, values]) => (
       <div key={key} style={{marginBottom: '5px'}}>
         <label>
-          <input type="checkbox" onChange={() => handleChange(name, key)} />
+          <input type="checkbox" checked={formValues[key]} onChange={() => handleChange(name, key)} />
           {values.map((kana) => `${kana.romaji} `)}
         </label>
       </div>
     ))}
+    <label>
+      <input type="checkbox" checked={formValues.all} onChange={(e) => selectAll(name, e.target.checked)} />
+      All {name}
+    </label>
   </div>
 )
